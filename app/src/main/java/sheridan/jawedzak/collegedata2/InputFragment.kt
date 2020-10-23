035f3c3b7411d382b22ccc1f235fce878369cdc2
Asad Jawed Zaki <jawedzak@sheridancollege.ca>
2020-10-23 15:18:22 -0400

    - Added Input and Output fragments - added layouts for both fragment - Edited nav.graph and added some Args

--- app/src/main/java/sheridan/jawedzak/collegedata2/InputFragment.kt
+++ app/src/main/java/sheridan/jawedzak/collegedata2/InputFragment.kt
@@ -5,27 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import sheridan.jawedzak.collegedata2.databinding.FragmentInputBinding
+import sheridan.jawedzak.collegedata2.model.CollegeData
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class InputFragment : Fragment() {
 
+    private lateinit var binding: FragmentInputBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false)
+        binding = FragmentInputBinding.inflate(inflater,container,false)
+
+        binding.btnSubmit.setOnClickListener { submit() }
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun submit(){
 
-        view.findViewById<Button>(R.id.btnSubmit).setOnClickListener {
-            findNavController().navigate(R.id.action_InputFragment_to_OutputFragment)
+        val studentName = binding.txtStudentName.text.toString()
+        var program = ""
+
+        program = if (binding.chkDiploma.isChecked){
+            "Diploma"
         }
+        else {
+            "Degree"
+        }
+
+        if (studentName.isNotEmpty() ){
+            showOutput(CollegeData(studentName,program))
+        }
+        else {
+            Toast.makeText(this.context, "Student name field is required", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showOutput(cData : CollegeData){
+        val action = InputFragmentDirections.actionInputToOutput(cData)
+        findNavController().navigate(action)
     }
 }
--- app/src/main/java/sheridan/jawedzak/collegedata2/OutputFragment.kt
+++ app/src/main/java/sheridan/jawedzak/collegedata2/OutputFragment.kt
@@ -5,27 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import sheridan.jawedzak.collegedata2.databinding.FragmentOutputBinding
+import sheridan.jawedzak.collegedata2.model.CollegeData
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class OutputFragment : Fragment() {
+    private lateinit var binding : FragmentOutputBinding
+    private lateinit var cData : CollegeData
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_output, container, false)
+        binding = FragmentOutputBinding.inflate(inflater,container,false)
+
+        //get parameters from safe args or bundle
+        cData =
+                if (savedInstanceState is Bundle){
+                    savedInstanceState.getSerializable("college_data") as CollegeData
+                }else{
+                    val safeargs: OutputFragmentArgs by navArgs()
+                    safeargs.cData
+                }
+
+        binding.lblStudentNme.text = cData.stuName
+        binding.lblProgram.text = cData.program
+
+
+        binding.btnBack.setOnClickListener { goBack() }
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.btnBack).setOnClickListener {
-            findNavController().navigate(R.id.action_OutputFragment_to_InputFragment)
-        }
+    private fun goBack(){
+        val action = OutputFragmentDirections.actionOutputToInput()
+        findNavController().navigate(action)
     }
 }
